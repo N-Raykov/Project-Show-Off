9cc8acfbd5daa2b988e62ceb9c982d3a07c17813
Nikola <nikolaradoslavovraykov@gmail.com>
2024-06-12 15:45:02 +0200

    Minor checkpoint change
    No longer creates a child object outside of runtime (do it yourself)

--- Assets/Scripts/CheckpointSystem/Checkpoint.cs
+++ Assets/Scripts/CheckpointSystem/Checkpoint.cs
@@ -1,14 +1,8 @@
 using UnityEngine;
 
-[ExecuteAlways]
 public class Checkpoint : MonoBehaviour
 {
     public GameObject respawnPoint;
-
-    private void Awake()
-    {
-        CreateChildGameObject();
-    }
 
     private void OnDrawGizmos()
     {
@@ -16,26 +10,11 @@ public class Checkpoint : MonoBehaviour
         Gizmos.DrawLine(transform.position, respawnPoint.transform.position);
     }
 
-    private void CreateChildGameObject()
-    {
-        if (respawnPoint != null)
-        {
-            return;
-        }
-
-        respawnPoint = new GameObject("RespawnPoint");
-
-        respawnPoint.transform.SetParent(this.transform);
-
-        respawnPoint.transform.localPosition = Vector3.zero; 
-    }
-
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             other.GetComponent<PlayerRespawn>().activeRespawnPoint = respawnPoint.transform.position;
-            Debug.Log("poggers");
         }
     }
 }
